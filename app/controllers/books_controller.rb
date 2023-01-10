9f0f7d7bda304954713a27ea88fa12c730192cdf
hahunavth <vuthanhha.2001@gmail.com>
2023-01-10 13:41:08 +0700

    Transition to Book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+
   before_action :set_book, only: %i[ show edit update destroy ]
 
   # GET /books or /books.json
